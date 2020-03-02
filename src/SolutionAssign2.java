785b07cea8773222bb34872d9c36562697ce5604
imfelton <isaiahfelton17@gmail.com>
2020-03-02 13:28:12 -0600

    Completed taking file name

--- src/SolutionAssign2.java
+++ src/SolutionAssign2.java
@@ -3,6 +3,10 @@ public class SolutionAssign2 {
 
         System.out.println(" “I affirm that I have not given or received any unauthorized help on this assignment, " +
                 "and that this work is my own.” ");
-        System.out.println(" My Name: ");
+        System.out.println(" My Name: Isaiah ");
+
+        System.out.println("Enter the file name: ");
+        Scanner input = new Scanner(System.in);
+        String name = input.next();
     }
 }
