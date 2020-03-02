ae5a35c9839fc0552d6e86a7307a3d7f24d8bc52
imfelton <isaiahfelton17@gmail.com>
2020-03-02 13:29:39 -0600

    Completed taking file name

--- src/SolutionAssign2.java
+++ src/SolutionAssign2.java
@@ -1,10 +1,10 @@
+import java.util.Scanner;
 public class SolutionAssign2 {
     public static void main(String[] args) {
 
         System.out.println(" “I affirm that I have not given or received any unauthorized help on this assignment, " +
                 "and that this work is my own.” ");
         System.out.println(" My Name: Isaiah ");
-
         System.out.println("Enter the file name: ");
         Scanner input = new Scanner(System.in);
         String name = input.next();
