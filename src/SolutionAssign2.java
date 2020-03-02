86aa4dc4f8795257ba80bbea8d55dd7f2fd166a7
gandhiUH <gvelusamy@uh.edu>
2020-03-02 11:55:19 -0600

    Initial commit

---
+++ src/SolutionAssign2.java
@@ -0,0 +1,8 @@
+public class SolutionAssign2 {
+    public static void main(String[] args) {
+
+        System.out.println(" “I affirm that I have not given or received any unauthorized help on this assignment, " +
+                "and that this work is my own.” ");
+        System.out.println(" My Name: ");
+    }
+}
